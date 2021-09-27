7f00ec5c6e5dc26eb243e42ad3e28928f8ddd3fa
Vladimir Salvador <alonsovla@gmail.com>
2021-09-27 14:15:46 -0500

    fix(docs): force deploy

--- apps/backoffice/frontend/tailwind.config.js
+++ apps/backoffice/frontend/tailwind.config.js
@@ -1,6 +1,12 @@
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
@@ -8,4 +14,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
