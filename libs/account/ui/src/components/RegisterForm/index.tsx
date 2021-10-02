3d98bfc3bb17eda9828731fc5fc2ebb10feb959d
Vladimir Salvador <alonsovla@gmail.com>
2021-10-01 20:35:57 -0500

    feat(backoffice/frontend): modify libs
    Changes by Raul Rosario

--- libs/account/ui/src/components/RegisterForm/index.tsx
+++ libs/account/ui/src/components/RegisterForm/index.tsx
@@ -6,16 +6,16 @@ export const RegisterForm = () => {
   const { onSubmit } = useRegisterForm();
 
   return (
-    <Form className="flex flex-col w-full p-4" onSubmit={onSubmit}>
-      <TextField label="Email:" type="email" name="email" validations={{ required: 'email is required' }} />
-      <TextField label="Name:" name="name" validations={{ required: 'name is required' }} />
-      <TextField label="Password:" type="password" name="password" validations={{ required: 'password is required', minLength: {
+    <Form className="flex flex-col w-full p-8" onSubmit={onSubmit}>
+      <TextField label="Email" type="email" name="email" validations={{ required: 'email is required' }} />
+      <TextField label="Name" name="name" validations={{ required: 'name is required' }} />
+      <TextField label="Password" type="password" name="password" validations={{ required: 'password is required', minLength: {
           value: 6,
           message: 'min length is 6',
         } }} />
-      <TextField label="Confirm Password:" type="password" name="confirm-password" validations={{ required: 'password confirm is required' }} />
+      <TextField label="Confirm Password" type="password" name="confirm-password" validations={{ required: 'password confirm is required' }} />
 
-      <Button title="Crear cuenta" type="submit" variant="cta" className={"my-4"} />
+      <Button title="Crear cuenta" type="submit" variant="cta" className={"mt-4"} />
     </Form>
   )
 }
