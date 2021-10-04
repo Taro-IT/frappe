64d027b6556cc611c12b142134cf4c0d9da762c1
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-10-03 23:00:11 -0500

    fix(category): fix build errors

--- libs/category/application/src/list/index.ts
+++ libs/category/application/src/list/index.ts
@@ -1,3 +1,3 @@
 export {ListCategoryQuery} from "./ListCategoryQuery"
-export { ListCategoryQueryHandler, ListCategoryQueryHandlerResult } from "./ListCategoryQueryHandler"
+export { ListCategoryQueryHandler } from "./ListCategoryQueryHandler"
 export { CategoryLister } from "./CategoryLister"
