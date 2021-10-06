37c49573f6c87b2e055b47bb5edf77ead5d8ad66
Vladimir Salvador <alonsovla@gmail.com>
2021-10-05 20:33:46 -0500

    build(backoffice/frontend): force deploy

--- apps/api-gateway/src/app.ts
+++ apps/api-gateway/src/app.ts
@@ -7,13 +7,14 @@ interface AppProps {
   readonly router: express.Router;
 }
 
+
 export const configureApp = ({ router }: AppProps): express.Express => {
   const app = express();
 
   app.use(cors()); // CORS
   app.use(helmet()); // Security Headers
   app.use(express.json()); // Response JSON
-  
+
   app.use('/api', router);
   app.use(errorHandler);
 
