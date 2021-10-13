5b3a32ac8c5524fe3aba367c5463424739fee7fb
Vladimir Salvador <alonsovla@gmail.com>
2021-10-13 16:52:14 -0500

    fix(github): add parse env variable

--- apps/api-gateway/src/container/commonDependencies.ts
+++ apps/api-gateway/src/container/commonDependencies.ts
@@ -11,7 +11,7 @@ const connectFirebase = () =>
   admin.initializeApp({
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
     })
   })
