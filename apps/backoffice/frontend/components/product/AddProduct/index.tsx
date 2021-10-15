f18d5cb8de4dbae4f399e98fa18c5e391f25b750
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-10-14 19:47:35 -0500

    feat: begin frontend form

---
+++ apps/backoffice/frontend/components/product/AddProduct/index.tsx
@@ -0,0 +1,17 @@
+import {Card} from '@frappe/common/design-system'
+import {AddProductForm} from "@frappe/account/ui";
+
+
+const AddProduct = () => {
+  return (
+    <Card className="w-50">
+        <Card.Header title="Crea un nuevo producto"/>
+          <AddProductForm />
+        <Card.Footer>
+        </Card.Footer>
+
+    </Card>
+  )
+}
+
+export default AddProduct
