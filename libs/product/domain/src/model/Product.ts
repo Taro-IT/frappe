9d437b2a9ba57cbd2a7bd65d3c3487b306267552
Vladimir Salvador <alonsovla@gmail.com>
2021-10-14 20:22:00 -0500

    feat: add ProductRepository

--- libs/product/domain/src/model/Product.ts
+++ libs/product/domain/src/model/Product.ts
@@ -1,6 +1,7 @@
-import { ProductName, ProductPrice } from '.';
-import { ProductPrimitives } from '../utils/ProductPrimitives';
+import { ProductPrimitives } from '../utils';
 import { ProductId } from './ProductId';
+import { ProductName } from './ProductName';
+import { ProductPrice } from './ProductPrice';
 
 export class Product {
   constructor(readonly id: ProductId, readonly name: ProductName, readonly price: ProductPrice) {}
---
+++ libs/product/domain/src/utils/ProductRepository.ts
@@ -0,0 +1,6 @@
+import {Product} from "../model";
+import {Criteria} from "@dinnosc/criteria";
+
+export interface ProductRepository {
+  search(criteria: Criteria<Product>): Promise<Product[]>;
+}
