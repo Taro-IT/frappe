09d6a95bf901d79c610dd605374a6777533ba5a6
Mauricio Alvarez <maualvm@gmail.com>
2021-10-25 11:39:18 -0500

    fix: cleanup admin layour

--- apps/backoffice/frontend/layouts/AdminLayout.tsx
+++ apps/backoffice/frontend/layouts/AdminLayout.tsx
@@ -1,25 +1,18 @@
 import { Sidebar, SidebarMobile } from '@frappe/common/design-system'
 import React from 'react'
 import { getSelectorsByUserAgent } from 'react-device-detect'
+import clsx from 'clsx';
 
 export const AdminLayout = ({children, sa}) => {
   const { isMobile } = getSelectorsByUserAgent(sa)
+
   return (
-    isMobile ?
-    (
-      <div className="bg-gray-100 w-full h-screen flex flex-col">
-        <SidebarMobile />
-        <div className="flex flex-col flex-grow pt-20">
+    <div className={ clsx('bg-gray-100 w-full h-screen flex', isMobile ? 'flex-col' : 'flex-row') }>
+      { isMobile ? <SidebarMobile /> : <Sidebar /> }
+
+      <div className="flex flex-col flex-grow pt-20">
           {children}
         </div>
-      </div>
-    ) : (
-      <div className="bg-gray-100 w-full h-screen flex flex-row">
-        <Sidebar />
-        <div className="flex flex-col flex-grow pt-20">
-          {children}
-        </div>
-      </div>
-    )
-  )
+    </div>
+  );
 }
