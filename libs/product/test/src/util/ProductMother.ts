0d2dd9b83f937ee93ea9ff423919bfdb5816dfe3
Vladimir Salvador <alonsovla@gmail.com>
2021-11-01 16:13:29 -0600

    fix(product): add missing attributes to ProductMother

--- libs/product/test/src/util/ProductMother.ts
+++ libs/product/test/src/util/ProductMother.ts
@@ -1,13 +1,30 @@
 import { UuidMother, StringMother, NumberMother} from "@frappe/common/test";
-import { Product } from "@frappe/product/domain";
+import {
+  Product, ProductAmount,
+  ProductCategories,
+  ProductDescription,
+  ProductImages,
+  ProductIsCustom,
+  ProductIsInSale, ProductIsLimited, ProductIsOutOfStock, ProductMaterials, ProductSizes
+} from '@frappe/product/domain';
 import { ProductId, ProductName, ProductPrice } from "@frappe/product/domain";
 
 export class ProductMother {
-    static random(): Product {
-        return new Product(
-            new ProductId(UuidMother.random()),
-            new ProductName(StringMother.randomWord()),
-            new ProductPrice(NumberMother.random())
-            )  
-    }
+  static random(): Product {
+    return new Product(
+      new ProductId(UuidMother.random()),
+      new ProductName(StringMother.randomWord()),
+      new ProductPrice(NumberMother.random()),
+      new ProductCategories([UuidMother.random()]),
+      new ProductDescription(StringMother.randomWord()),
+      new ProductImages([StringMother.url()]),
+      new ProductIsCustom(false),
+      new ProductIsInSale(false),
+      new ProductIsLimited(false),
+      new ProductIsOutOfStock(false),
+      new ProductMaterials([StringMother.randomWord()]),
+      new ProductSizes([NumberMother.random()]),
+      new ProductAmount(NumberMother.random()),
+    )
+  }
 }
