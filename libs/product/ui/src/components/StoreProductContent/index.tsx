8d9b94297a4d9d506d095606bbd290d92e1a925c
RGalaviz <ralsandoval16@gmail.com>
2021-11-01 22:41:19 -0600

    feat: add component pagination

--- libs/product/ui/src/components/StoreProductContent/index.tsx
+++ libs/product/ui/src/components/StoreProductContent/index.tsx
@@ -4,21 +4,19 @@ import clsx from 'clsx';
 import styles from './StoreProductContent.module.scss';
 import { useProducts } from '../../hooks';
 import { ProductList } from '..';
+import { Pagination } from '@frappe/common/design-system';
 
 interface StoreProductContentProps {
   readonly className: string;
 }
 
 export const StoreProductContent = ({ className }: StoreProductContentProps) => {
-  const { products } = useProducts();
-
-  console.log(products);
-
+  const { products, total} = useProducts();
   return (
     <div className={ clsx(className, styles.wrapper) }>
       <div className={ styles['wrapper--content'] }>
         <ProductList products={products}/>
-        { /* TODO Component Pagination */ }
+        <Pagination total={total} itemsPerPage={20}/>
       </div>
     </div>
   )
