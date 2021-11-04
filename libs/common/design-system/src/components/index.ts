add42766b115eb2cff8f3ba7e92999a0f26210eb
EricBL3 <ericbuitron3@gmail.com>
2021-11-04 17:10:25 -0600

    feat: add summaries example template

--- libs/common/design-system/src/components/index.ts
+++ libs/common/design-system/src/components/index.ts
@@ -3,3 +3,4 @@ export { Form } from './Form';
 export { Link } from './Link';
 export { SpanError } from './SpanError';
 export { TextField } from './TextField';
+export { SummariesExample } from './SummariesExample'; 
