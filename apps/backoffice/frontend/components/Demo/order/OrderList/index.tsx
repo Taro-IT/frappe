fa16da35fb610eb21db4f00378be8307ebb67a2f
EricBL3 <ericbuitron3@gmail.com>
2021-11-04 11:10:11 -0600

    feat: add steps and summaries templates
    We still need to add the alerts and badges components (they can be found
    in frappe-overflow).
    The components are just the code from the examples, we need to abstract them to use them properly in our system.

--- apps/backoffice/frontend/components/Demo/order/OrderList/index.tsx
+++ apps/backoffice/frontend/components/Demo/order/OrderList/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import OrderCard from './OrderCard';
+import { Steps, Summaries } from '@frappe/common/design-system';
 
 const OrderList = () => {
   const [orders, setOrders] = useState([]);
@@ -27,6 +28,8 @@ const OrderList = () => {
 
   return (
     <div className="overflow-y-scroll w-full">
+      <Steps />
+      <Summaries />
       <div className="w-full  ">{useOrders.length ? useOrders : 'No tienes órdenes registradas.'}</div>
     </div>
   );
--- libs/common/design-system/src/components/index.ts
+++ libs/common/design-system/src/components/index.ts
@@ -2,3 +2,5 @@ export { Form } from './Form';
 export { Link } from './Link';
 export { SpanError } from './SpanError';
 export { TextField } from './TextField';
+export { Steps } from './Steps';
+export { Summaries } from './Summaries';
