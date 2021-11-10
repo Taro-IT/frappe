13e6412672fbf01857f2537d9aa9b97efc74142f
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-11-09 21:40:05 -0600

    feat: use states for inputs

--- apps/backoffice/frontend/components/product/components/AddProductForm/index.tsx
+++ apps/backoffice/frontend/components/product/components/AddProductForm/index.tsx
@@ -1,80 +1,110 @@
-import {Button, Form, TextField} from "@frappe/common/design-system";
-import Select from 'react-select'
-import { useEffect, useState } from "react";
+import { Button, Form, TextField } from '@frappe/common/design-system';
+import Select from 'react-select';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import styles from './AddProductForm.module.scss'
-import "react-toggle/style.css"
-import Toggle from 'react-toggle'
-import SizeSelector from '../SizeSelector'
+import styles from './AddProductForm.module.scss';
+import 'react-toggle/style.css';
+import Toggle from 'react-toggle';
+import SizeSelector from '../SizeSelector';
 
 const AddProductForm = () => {
   const [, setCategories] = useState();
   const [options, setOptions] = useState();
   const [isLimited, setIsLimited] = useState<boolean>(false);
   const [isCustom, setIsCustom] = useState<boolean>(false);
+  const [amount, setAmount] = useState<number>()
+  const [sizes, setSizes] = useState<number[]>([]);
+  const [price, setPrice] = useState<number>();
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
-  const sizes = ["22.5", "23", "23.5","24","24.5", "25", "25.5", "26", "26.5"]
+  const [productName, setProductName] = useState<string>();
+
+  const [productDescription, setProductDescription] = useState<string>('');
+
+  const defaultSizes = ['22.5', '23', '23.5', '24', '24.5', '25', '25.5', '26', '26.5'];
 
   type Category = {
-    id: string,
-    name : string,
-  }
+    id: string;
+    name: string;
+  };
 
-  
   useEffect(() => {
     const getCategories = async (): Promise<void> => {
       const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
       const data = response.data.result;
       if (data.length !== 0) {
         setCategories(data);
-        setOptions(data.map((option: Category) =>
-          {return {value: option.id, label: option.name}}
-          ));
+        setOptions(
+          data.map((option: Category) => {
+            return { value: option.id, label: option.name };
+          })
+        );
       }
     };
     getCategories();
   }, []);
 
   const customStyles = {
-    option: (provided: any, state: { isSelected: any; }) => ({
+    option: (provided: any, state: { isSelected: any }) => ({
       ...provided,
-      backgroundColor: 
-        state.isSelected
-        ? "rgb(163, 142, 101)"
-        : undefined,
-      color: 
-        state.isSelected
-        ? "rgb(163, 142, 101)"
-        : undefined
+      backgroundColor: state.isSelected ? 'rgb(163, 142, 101)' : undefined,
+      color: state.isSelected ? 'rgb(163, 142, 101)' : undefined
     })
-  }
+  };
 
-  const handleProductToggle = () => {
-    setIsLimited(previous => !previous)
-  }
-
-
+  const handleStockChange = () => {
+    setIsLimited(previous => !previous);
+  };
+  const handleCustomChange = () => {
+    setIsCustom(previous => !previous);
+  };
 
   const submitProduct = async data => {
     console.log(data);
-    
-    
+
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/products/`, {
       ...data,
-      isOutOfStock: false,
-      /* isCustom: //custom, // TODO: add `custom` state
-      isLimited: */
-    })
-    
-  }
+      isOutOfStock: false
+    });
+  };
+
+  const changePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(parseInt(event.target.value));
+  };
+
+  const changeProductDescription = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setProductDescription(event.target.value);
+  };
+
+  const handleSelectCategories = selectedOptions => {
+    const selected = selectedOptions.map(opt => opt.value);
+    setSelectedCategories(selected);
+  };
+
+  const handleProductName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProductName(e.target.value);
+  };
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(parseInt(e.target.value));
+  };
 
   return (
     <Form className="flex flex-col w-full p-8" onSubmit={submitProduct}>
-      <TextField label="Nombre del producto" type="name" name="name" validations={{ required: 'El nombre del producto es requerido' }} />
-      <TextField label="Materiales disponibles" name="materials" validations={{ required: 'Los materiales son requeridos' }} />
-      <label className={'w-1/3 mt-4 mb-3'}>
-          Categoría(s)
-      </label>
+      <div className="flex flex-col">
+        <label className='w-1/3 mt-4 mb-2'>Nombre del producto</label>
+        <input
+          value={productName}
+          onChange={handleProductName}
+          placeholder="Bandolera"
+          className="border-2 border-gray-200 rounded pl-2 w-full h-8"
+        />
+      </div>
+
+      {/* Se comenta esta parte hasta que se defina la personalización
+        <TextField label="Materiales disponibles" name="materials" validations={{ required: 'Los materiales son requeridos' }} /> 
+      */}
+
+      <label className={'w-1/3 mt-4 mb-3'}>Categoría(s)</label>
       <Select
         isMulti
         name="categories"
@@ -82,58 +112,63 @@ const AddProductForm = () => {
         className="basic-multi-select"
         classNamePrefix="Selecciona algunas categorías"
         styles={customStyles}
+        onChange={handleSelectCategories}
       />
-      <label className='w-1/3 mt-4 mb-3'>
-          Tallas disponibles
-      </label>
+      <label className="w-1/3 mt-4 mb-3">Tallas disponibles</label>
       <div className="flex flex-row space-x-4 mb-4">
-        {sizes.map(size => 
-          <SizeSelector size={size} />
-        )}
+        {defaultSizes.map(size => (
+          <SizeSelector setSizesArray={setSizes} sizesArray={sizes} size={parseFloat(size)} />
+        ))}
       </div>
-      
-      <TextField label="Precio" type="number" name="price" validations={{ required: 'El precio es requerido'}}/>
-      <div className="flex flex-row">
-        <label className='w-auto mr-4 mt-4 mb-3'>
-            ¿Este producto tiene stock?
-        </label>
-        <Toggle
-              defaultChecked={isLimited}
-              icons={false}
-              className='mt-4'
-              onChange={handleProductToggle} 
+
+      <div className="flex flex-col">
+        <label className='w-1/3 mt-4 mb-2'>Precio</label>
+        <input
+          value={price}
+          onChange={changePrice}
+          placeholder="3999.00"
+          type="number"
+          className="border-2 border-gray-200 h-8 rounded pl-2 w-full"
         />
       </div>
-      {isLimited && 
-          <TextField label="Cantidad" type="number" name="amount" validations={{ required: 'La cantidad es requerida'}}/>
-      }
-      
-      <label className='w-1/3 mt-4 mb-3'>
-          Descripción
-      </label>
-      <textarea className='border-2 border-gray-200 rounded w-full' />
-      
-      <label className='w-1/3 mt-4 mb-3'>
-          Imágenes
-      </label>
-      <input className={styles['input-file']} type="file" id="files" name="files" multiple/>
       <div className="flex flex-row">
-        <label className={'w-auto mt-4 mb-3 mr-4'}>
-            ¿Este producto es personalizable?
-        </label>
-        <Toggle
-            defaultChecked={false}
-            icons={false}
-            className='mt-4'
-            onChange={()=> {}} 
-          />
-
+        <label className="w-auto mr-4 mt-4 mb-3">¿Este producto tiene stock?</label>
+        <Toggle defaultChecked={isLimited} icons={false} className="mt-4" onChange={handleStockChange} />
       </div>
+      {isLimited && (
+        <div className="flex flex-col">
+        <label className='w-1/3 mt-2 mb-2'>Cantidad</label>
+        <input
+          value={amount}
+          onChange={handleAmountChange}
+          placeholder="5"
+          type="number"
+          className="border-2 border-gray-200 h-8 rounded pl-2 w-full"
+        />
+      </div>
+      )}
 
+      <label className="w-1/3 mt-4 mb-3">Descripción</label>
 
-      <Button title="Agregar producto" type="submit" variant="cta" className={"mt-4"} />
+      <textarea
+        value={productDescription}
+        className="border-2 border-gray-200 rounded w-full"
+        onChange={changeProductDescription}
+      />
+      <label className="w-1/3 mt-4 mb-3">Imágenes</label>
+      <input className={styles['input-file']} type="file"  id="files" name="files" multiple />
+      <p className="text-xs text-red-500 mt-2">Puedes subir varias imágenes haciendo click en Ctrl (Cmd en Mac) mientras seleccionas.</p>
+      <div className="flex flex-row">
+        <label className={'w-auto mt-4 mb-3 mr-4'}>¿Este producto es personalizable?</label>
+        <Toggle defaultChecked={isCustom} icons={false} onChange={handleCustomChange} className="mt-4" />
+      </div>
+      {isCustom && (
+        <p className='text-center text-gray-400 w-1/3 mt-4 mb-2'>AQUI VAN A IR LOS CAMPOS DE CUSTOMIZACIÓN</p>
+      )}
+
+      <Button title="Agregar producto" type="submit" variant="cta" className={'mt-4'} />
     </Form>
-  )
-}
+  );
+};
 
-export default AddProductForm
+export default AddProductForm;
--- apps/backoffice/frontend/components/product/components/SizeSelector/index.tsx
+++ apps/backoffice/frontend/components/product/components/SizeSelector/index.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react"
 
 type SizeSelectorProps = {
-  size: string
+  size: number
+  setSizesArray: (nv: number[]) => any
+  sizesArray: number[]
 }
 
-const SizeSelector = ({size}: SizeSelectorProps) => {
+const SizeSelector = ({size, setSizesArray, sizesArray}: SizeSelectorProps) => {
 
   const [isSelected, setIsSelected] = useState<boolean>(false)
 
   const toggleSelected = () => {
     setIsSelected( previous => !previous)
+    if (!isSelected) {
+      setSizesArray([...sizesArray, size])
+    }else{
+      setSizesArray(sizesArray.filter(sz => sz != size))
+    }
   }
 
   return <div className={`w-20 h-10 cursor-pointer rounded-md justify-content-center border-2 ${isSelected ? 'border-yellow-400' : 'border-gray-300'} `} onClick={toggleSelected}>
