33ae8523fac002e6ac7ea3d933b3c0374e6e1c77
Vladimir Salvador <alonsovla@gmail.com>
2021-11-13 23:38:24 -0600

    feat(users): add user finder

--- libs/account/domain/src/utils/user/UserRepository.ts
+++ libs/account/domain/src/utils/user/UserRepository.ts
@@ -1,5 +1,7 @@
-import { User } from '../../model';
+import { User, UserId } from '../../model';
+import { Nullable } from '@frappe/common/utils';
 
 export interface UserRepository {
   save(user: User): Promise<void>;
+  find(id: UserId): Promise<Nullable<User>>;
 }
--- libs/account/persistence/mongodb/src/utils/MongoUserRepository.ts
+++ libs/account/persistence/mongodb/src/utils/MongoUserRepository.ts
@@ -1,5 +1,6 @@
 import { MongoRepository } from '@frappe/common/persistence/mongodb';
-import { User, UserRepository } from '@frappe/account/domain';
+import { User, UserId, UserPrimitives, UserRepository } from '@frappe/account/domain';
+import { Nullable } from '@frappe/common/utils';
 
 export class MongoUserRepository extends MongoRepository implements UserRepository {
   protected moduleName(): string {
@@ -8,5 +9,22 @@ export class MongoUserRepository extends MongoRepository implements UserReposito
 
   save(user: User): Promise<void> {
     return this.persist(user.id.value, user);
+  }
+
+  /**
+   * Finds a user @see {@link User}
+   * @param id - The id of the user you want to find
+   *
+   * @returns the found user or null if it does not exist
+   */
+  async find(id: UserId): Promise<Nullable<User>> {
+    const collection = await this.collection();
+    const document = await collection.findOne({ _id: id.value });
+
+    if (!document) {
+      return null;
+    }
+
+    return User.fromPrimitives({ ...document, id: document._id } as UserPrimitives);
   }
 }
