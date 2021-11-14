8efbebb5f1c881ab06ce30f3d1e7a28449210da9
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-11-13 21:25:10 -0600

    feat: finish frontend

--- apps/backoffice/frontend/components/material/AddMaterial/index.tsx
+++ apps/backoffice/frontend/components/material/AddMaterial/index.tsx
@@ -1,34 +1,51 @@
-import { Button, FileInput, Card } from '@frappe/common/design-system';
+import { Button, FileInput, Card, Modal } from '@frappe/common/design-system';
 import classes from './AddMaterial.module.scss';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { BadgeCheckIcon, ExclamationIcon } from '@heroicons/react/solid';
 
 const CreateMaterial = () => {
   const [materialName, setMaterialName] = useState<string>('');
   const [file, setFile] = useState<File>();
-  const [fileURL, setFileURL] = useState<string>('');
+  const [showRetroModal, setShowRetroModal] = useState<boolean>(false)
+  const [success, setSuccess] = useState<boolean>();
+  const [message, setMessage] = useState<string>()
+  const [loading, setLoading] = useState<boolean>(false)
+  const[clear, setClear] = useState<boolean>(false)
 
   const addMaterialHandler = async event => {
     event.preventDefault();
+    setClear(false)
+    if (loading === true) {return}
+    setLoading(true)
     if (materialName == '') return;
 
-
     try {
-      
       // Post de imágenes
       const bodyFormData = new FormData();
       bodyFormData.append('file', file);
+      console.table(bodyFormData.getAll('file'));
       const { data: { name } } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/file-system/`, bodyFormData);
-      setFileURL(name)
-
+      
       await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/materials/`, {
         name: materialName,
-        image: fileURL
+        image: name
       });
+      setShowRetroModal(true)
+      setSuccess(true)
+      setMessage("Material creado correctamente")
+      setLoading(false)
+      setClear(true)
+      
     } catch (error) {
-      console.error('El material ya existe.');
+      setShowRetroModal(true)
+      setSuccess(false)
+      setMessage("Este material ya existe en la base de datos, intenta crear un nuevo material")
+      console.error("El material ya existe");
+      setClear(true)
+      setLoading(false)
+      
     }
-
     setMaterialName('');
   };
 
@@ -37,15 +54,26 @@ const CreateMaterial = () => {
   };
 
   return (
+    <>
     <Card className={classes.input}>
       <form>
         <label htmlFor="materialName">Nombre del material</label>
         <input className=" my-2 border-2 border-gray-200 rounded pl-2 w-full h-10" id="materialName" placeholder="Piel roja" type="text" value={materialName} onChange={nameChangeHandler} />
         <label htmlFor="materialName">Imágen del material</label>
-        <FileInput multiple={false} required={true}/>
+        <FileInput setFiles={setFile} clear={clear} multiple={false} required={true}/>
         <Button variant="cta" type="submit" onClick={addMaterialHandler} title="Agregar" />
       </form>
     </Card>
+    {showRetroModal && (
+      <Modal showModal={showRetroModal} toggleModal={setShowRetroModal} title="">
+        <div className="flex flex-col w-full px-20 mb-4 -mt-10 justify-center items-center">
+          {success && <BadgeCheckIcon className="items-center h-32 w-32 text-green-400 mb-6" />}
+          {!success && <ExclamationIcon className="items-center h-32 w-32 text-red-500 mb-6" />}
+          <p className="text-2xl text-center mb-4">{message}</p>
+        </div>
+      </Modal>
+    )}
+    </>
   );
 };
 
--- libs/common/design-system/src/components/FileInput/index.tsx
+++ libs/common/design-system/src/components/FileInput/index.tsx
@@ -1,32 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type FileInputProps = {
   multiple: boolean
   required: boolean
+  setFiles: (nv: any) => any
+  clear: boolean
 }
 
-export const FileInput = ({multiple, required}: FileInputProps) => {
+export const FileInput = ({multiple, required, setFiles, clear}: FileInputProps) => {
   const [isFileSet, setFile] = useState<boolean>(false)
-  const [fileNames, setFileNames] =useState<string[]>()
+  const [names, setFileNames] =useState<string[]>()
+  const [inputKey, setInputKey] = useState<string>()
+
+  useEffect(() => {
+    if(clear === true) {
+      let randomString = Math.random().toString(36);
+      setInputKey(randomString)
+      setFile(false)
+    }
+  }, [clear])
   
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFile( pv => !pv)
-    const files = e.target.files;
-    console.log(files);
-    if (files === null || files === undefined) return
-    for (let i = 0; i < files.length; i++) {
-      let file = files.item(i);
-      setFileNames(pv => [...pv, file.name])
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(pv => !pv)
+    if (!e || !e.target || !e.target.files || e.target.files.length === 0) {
+      return;
+    }
+    if (!multiple) {
+      const name = e.target.files[0].name;
+      setFileNames([name])
+      setFiles(e.target.files[0]);
+    }else{
+      /*TODO: VALIDATE IT WORKS FOR MANY FILES*/
+      const files = e.target.files;
+      const fileArray = Array.from(files);
+      const filesLength = files.length;
+      const fileNames : string[] = []
+      for(let i = 0; i < filesLength; i++ ){
+        const{ name } = files.item(i);
+        fileNames.push(name);
+      }
+      setFileNames(fileNames)
+      setFiles(fileArray);
     }
     
-    // if (files === null || files === undefined) return
-    // const filesLength = files.length;
-    // for(let i = 0; i < filesLength; i++ ){
-    //   const{ name } = files.item(i);
-    //   setFileNames(pv => [...pv, name] )
-    // }
   }
-
   return (
     <>
       <link
@@ -44,7 +61,7 @@ export const FileInput = ({multiple, required}: FileInputProps) => {
           tracking-wide
           uppercase
           h-auto
-          border-2 border-gray-200
+          border-dashed border-2 border-gray-200 
           cursor-pointer
           text-yellow-400
           ease-linear
@@ -53,9 +70,9 @@ export const FileInput = ({multiple, required}: FileInputProps) => {
           mb-2
         "
         >
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>        
-        <span className="text-base leading-normal">{isFileSet ? `Archivos cargados: ${fileNames}`: "Selecciona un archivo"}</span>
-        <input onChange={handleFileChange} multiple={multiple} type="file" className="hidden" required={required}/>
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>        
+        <span className="text-base leading-normal">{isFileSet ? `Archivos cargados: ${names}`: "Selecciona un archivo"}</span>
+        <input key={inputKey ? inputKey : ""} onChange={onChangeFile} multiple={multiple} type="file" className="hidden" required={required}/>
       </label>
     </>
   );
