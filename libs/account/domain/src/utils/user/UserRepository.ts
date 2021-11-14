7956fbe4e43c28d842c202fa502187882ba6de10
Vladimir Salvador <alonsovla@gmail.com>
2021-11-13 22:53:11 -0600

    feat(users): add product searcher

--- libs/account/domain/src/utils/user/UserRepository.ts
+++ libs/account/domain/src/utils/user/UserRepository.ts
@@ -1,5 +1,8 @@
 import { User } from '../../model';
+import { Criteria } from '@dinnosc/criteria';
 
 export interface UserRepository {
   save(user: User): Promise<void>;
+  search(criteria: Criteria<User>): Promise<User[]>;
+  total(): Promise<number>;
 }
--- libs/account/persistence/mongodb/src/utils/MongoUserRepository.ts
+++ libs/account/persistence/mongodb/src/utils/MongoUserRepository.ts
@@ -1,5 +1,6 @@
 import { MongoRepository } from '@frappe/common/persistence/mongodb';
-import { User, UserRepository } from '@frappe/account/domain';
+import { User, UserPrimitives, UserRepository } from '@frappe/account/domain';
+import { Criteria } from '@dinnosc/criteria';
 
 export class MongoUserRepository extends MongoRepository implements UserRepository {
   protected moduleName(): string {
@@ -8,5 +9,26 @@ export class MongoUserRepository extends MongoRepository implements UserReposito
 
   save(user: User): Promise<void> {
     return this.persist(user.id.value, user);
+  }
+
+  /**
+   * Search database users that matches the provided criteria
+   *
+   * @param criteria Object that represents a Query
+   */
+  async search(criteria: Criteria<User>): Promise<User[]> {
+    const users = await this.searchByCriteria(criteria);
+
+    return users.map(user => User.fromPrimitives({ ...user, id: user._id } as UserPrimitives));
+  }
+
+  /**
+   * Return the total collection record
+   *
+   */
+  async total(): Promise<number> {
+    const collection = await this.collection();
+
+    return collection.countDocuments();
   }
 }
