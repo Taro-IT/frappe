39a311d93332bb6d60864f9b3bb628a7ab5e1ea0
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-11-14 13:28:43 -0600

    test: add delete tests

---
+++ libs/material/application/src/delete/MaterialDeleter.spec.ts
@@ -0,0 +1,36 @@
+import { MaterialRepository, MaterialNotFound } from '@frappe/material/domain';
+import { MaterialDeleter } from './MaterialDeleter';
+import { MaterialFinder } from '../find';
+import { mock, MockProxy, DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { MaterialMother } from '@frappe/material/test';
+
+describe('MaterialDeleter', () => {
+  let materialRepository: MockProxy<MaterialRepository>;
+  let materialFinder: DeepMockProxy<MaterialFinder>;
+
+  let deleter: MaterialDeleter;
+
+  beforeEach(() => {
+    materialRepository = mock<MaterialRepository>();
+    materialFinder = mockDeep<MaterialFinder>(new MaterialFinder({ materialRepository }));
+
+    deleter = new MaterialDeleter({ materialRepository, materialFinder });
+  });
+
+  it('should delete a Material', async () => {
+    const material = MaterialMother.deleted();
+    materialRepository.find.mockResolvedValueOnce(material);
+    await deleter.execute(material.id.value);
+
+    expect(materialRepository.save).toHaveBeenCalledWith(material);
+  });
+
+  it('should throw a MaterialNotFound', async () => {
+    const material = MaterialMother.random();
+
+    materialRepository.find.mockRejectedValueOnce(MaterialNotFound);
+    const response = () => deleter.execute(material.id.value);
+
+    await expect(async () => response()).rejects.toThrow(MaterialNotFound);
+  });
+});
--- libs/material/test/src/utils/MaterialMother.ts
+++ libs/material/test/src/utils/MaterialMother.ts
@@ -1,8 +1,11 @@
-import { Material, MaterialId, MaterialName, MaterialImage} from '@frappe/material/domain';
-import { StringMother, UuidMother } from '@frappe/common/test';
+import { Material, MaterialId, MaterialName, MaterialImage, MaterialIsActive, MaterialDeletedAt} from '@frappe/material/domain';
+import { DateMother, StringMother, UuidMother } from '@frappe/common/test';
 
 export class MaterialMother {
   static random(): Material {
-    return new Material(new MaterialId(UuidMother.random()), new MaterialName(StringMother.random()), new MaterialImage(StringMother.random()));
+    return new Material(new MaterialId(UuidMother.random()), new MaterialName(StringMother.random()), new MaterialImage(StringMother.random()), new MaterialIsActive(true));
+  }
+  static deleted(): Material {
+    return new Material(new MaterialId(UuidMother.random()), new MaterialName(StringMother.random()), new MaterialImage(StringMother.random()), new MaterialIsActive(false), new MaterialDeletedAt(new Date));
   }
 }
