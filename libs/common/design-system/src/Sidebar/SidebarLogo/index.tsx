da0a307d2763faf504762026f06a40af100d7712
EricBL3 <ericbuitron3@gmail.com>
2021-11-17 11:34:09 -0600

    fix: modify SidebarLogo to fix defect

--- libs/common/design-system/src/Sidebar/SidebarLogo/index.tsx
+++ libs/common/design-system/src/Sidebar/SidebarLogo/index.tsx
@@ -1,15 +1,7 @@
-import { useRouter } from 'next/router';
-
 const SidebarLogo = () => {
-  const router = useRouter();
-
-  const goHome = () => {
-    router.push("/");
-  };
-  
   return(
     <div className="px-4">
-      <img className="m-auto" src="/img/cinica-logo.png" alt="Cínica Logo" onClick={goHome} />
+      <a href="/"><img className="m-auto" src="/img/cinica-logo.png" alt="Cínica Logo"/></a>
     </div>
   );
 };
--- libs/common/design-system/src/SidebarMobile/SidebarLogo/index.tsx
+++ libs/common/design-system/src/SidebarMobile/SidebarLogo/index.tsx
@@ -1,15 +1,7 @@
-import { useRouter } from 'next/router';
-
 const SidebarLogo = () => {
-  const router = useRouter();
-
-  const goHome = () => {
-    router.push("/");
-  };
-  
   return(
     <div className="ml-auto pr-2">
-    <img className="flex justify-end h-10 ml-auto" src="/img/cinica-logo.png" alt="Cínica Logo" onClick={goHome} />
+    <a href="/"><img className="flex justify-end h-10 ml-auto" src="/img/cinica-logo.png" alt="Cínica Logo" /></a>
   </div>
   );
 };
