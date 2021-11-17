8f2fb74157bbbb1d813c371f19f3f3a4177fb8f8
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-11-16 20:43:56 -0600

    Update libs/material/test/src/utils/MaterialMother.ts
    Co-authored-by: EricBL3 <43388801+EricBL3@users.noreply.github.com>

--- libs/material/test/src/utils/MaterialMother.ts
+++ libs/material/test/src/utils/MaterialMother.ts
@@ -3,7 +3,12 @@ import {StringMother, UuidMother } from '@frappe/common/test';
 
 export class MaterialMother {
   static random(): Material {
-    return new Material(new MaterialId(UuidMother.random()), new MaterialName(StringMother.random()), new MaterialImage(StringMother.random()), new MaterialIsActive(true));
+    return new Material(
+      new MaterialId(UuidMother.random()), 
+      new MaterialName(StringMother.random()), 
+      new MaterialImage(StringMother.random()), 
+      new MaterialIsActive(true)
+    );
   }
   static deleted(): Material {
     return new Material(new MaterialId(UuidMother.random()), new MaterialName(StringMother.random()), new MaterialImage(StringMother.random()), new MaterialIsActive(false), new MaterialDeletedAt(new Date()));
