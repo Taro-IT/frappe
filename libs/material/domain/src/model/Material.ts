724b1fa02cbced1a7324b59cb8d56ed0e1e3be6c
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-11-16 20:43:41 -0600

    Update libs/material/domain/src/model/Material.ts
    Co-authored-by: EricBL3 <43388801+EricBL3@users.noreply.github.com>

--- libs/material/domain/src/model/Material.ts
+++ libs/material/domain/src/model/Material.ts
@@ -8,7 +8,13 @@ import { MaterialIsActive } from './MaterialIsActive';
 import { MaterialDeletedAt } from './MaterialDeletedAt';
 
 export class Material {
-  constructor(readonly id: MaterialId, readonly name: MaterialName, readonly image: MaterialImage, readonly isActive: MaterialIsActive, readonly deletedAt?: MaterialDeletedAt) {}
+  constructor(
+    readonly id: MaterialId, 
+    readonly name: MaterialName, 
+    readonly image: MaterialImage, 
+    readonly isActive: MaterialIsActive, 
+    readonly deletedAt?: MaterialDeletedAt
+  ) {}
 
   static fromPrimitives(primitives: MaterialPrimitives): Material {
     return new Material(new MaterialId(primitives.id), new MaterialName(primitives.name), new MaterialImage(primitives.image), new MaterialIsActive(primitives.isActive), new MaterialDeletedAt(primitives.deletedAt));
