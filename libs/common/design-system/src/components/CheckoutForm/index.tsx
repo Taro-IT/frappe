b33f3eea0bfd8fa26d0a2feffeec7ab5435f54e9
JanLimpensG <jan_limpens@hotmail.com>
2021-11-23 12:16:47 -0600

    feat: correct shiping info form

--- libs/common/design-system/src/components/CheckoutForm/index.tsx
+++ libs/common/design-system/src/components/CheckoutForm/index.tsx
@@ -1,190 +1,191 @@
 export function CheckoutForm() {
 
   return (
-    <div className="bg-gray-100">
-      <div className="max-w-2xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Checkout</h2>
+  <div className="bg-gray-100">
+    <div className="max-w-2xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8 justify-sele-center">
+      <h2 className="sr-only">Checkout</h2>
 
-        <form className="lg:gap-x-12 xl:gap-x-16">
+      <form className="lg:gap-x-12 xl:gap-x-16">
+        <div>
           <div>
-            <div>
-              <h2 className="text-lg font-medium text-gray-900">Información de contacto</h2>
-
-              <div className="mt-4">
-                <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                  Correo electrónico
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="email"
-                    id="email-address"
-                    name="email-address"
-                    autoComplete="email"
-                    className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
+            <h2 className="text-lg font-medium text-gray-900">Información de contacto</h2>
+            
+            <div className="mt-4">
+              <label htmlFor="clientName" className="block text-sm font-medium text-gray-700">
+                Nombre del recipiente*
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  id="clientName"
+                  name="clientName"
+                  autoComplete="given-name"
+                  className="w-1/4 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
               </div>
             </div>
-
-            <div className="mt-10 border-t border-gray-200 pt-10">
-              <h2 className="text-lg font-medium text-gray-900">Shipping information</h2>
-
-              <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-                <div>
-                  <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                    First name
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      id="first-name"
-                      name="first-name"
-                      autoComplete="given-name"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                    Last name
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      id="last-name"
-                      name="last-name"
-                      autoComplete="family-name"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label htmlFor="company" className="block text-sm font-medium text-gray-700">
-                    Company
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="company"
-                      id="company"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label htmlFor="address" className="block text-sm font-medium text-gray-700">
-                    Address
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="address"
-                      id="address"
-                      autoComplete="street-address"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label htmlFor="apartment" className="block text-sm font-medium text-gray-700">
-                    Apartment, suite, etc.
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="apartment"
-                      id="apartment"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <label htmlFor="city" className="block text-sm font-medium text-gray-700">
-                    City
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="city"
-                      id="city"
-                      autoComplete="address-level2"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                    Country
-                  </label>
-                  <div className="mt-1">
-                    <select
-                      id="country"
-                      name="country"
-                      autoComplete="country-name"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    >
-                      <option>United States</option>
-                      <option>Canada</option>
-                      <option>Mexico</option>
-                    </select>
-                  </div>
-                </div>
-
-                <div>
-                  <label htmlFor="region" className="block text-sm font-medium text-gray-700">
-                    State / Province
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="region"
-                      id="region"
-                      autoComplete="address-level1"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">
-                    Postal code
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="postal-code"
-                      id="postal-code"
-                      autoComplete="postal-code"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-                    Phone
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="phone"
-                      id="phone"
-                      autoComplete="tel"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
+            <div className="mt-4">
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Correo electrónico*
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="email"
+                  id="email"
+                  name="email"
+                  autoComplete="email"
+                  className="w-1/4  border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+                Número telefónico*
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  autoComplete="tel"
+                  className=" w-1/4  border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
               </div>
             </div>
           </div>
-        </form>
-      </div>
+
+          <div className="mt-10 border-t border-gray-200 pt-10">
+            <h2 className="text-lg font-medium text-gray-900">Información de envío</h2>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="company" className="block text-sm font-medium text-gray-700">
+                  Compañía
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="company"
+                    id="company"
+                    className=" w-1/4  border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="address1" className="block text-sm font-medium text-gray-700">
+                  Dirección de envío principal*
+                </label>
+                <div className="mt-1">
+                  <input
+                    required
+                    type="text"
+                    name="address1"
+                    id="address1"
+                    autoComplete="street-address"
+                    className=" w-1/2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="address2" className="block text-sm font-medium text-gray-700">
+                  Dirección de envío secundaria
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="address2"
+                    id="address2"
+                    autoComplete="street-address"
+                    className=" w-1/2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="reference" className=" text-sm font-medium text-gray-700">
+                  Referencia
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="reference"
+                    id="reference"
+                    className="w-1/2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                  Ciudad*
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    autoComplete="address-level2"
+                    className="w-1/4 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                  País*
+                </label>
+                <div className="mt-1">
+                  <select
+                    required
+                    id="country"
+                    name="country"
+                    autoComplete="country-name"
+                    className="w-1/4 text-center border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
+                    <option>Mexico</option>
+                  </select>
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="province" className="block text-sm font-medium text-gray-700">
+                  Estado*
+                </label>
+                <div className="mt-1">
+                  <input
+                    required
+                    type="text"
+                    name="province"
+                    id="province"
+                    autoComplete="address-level1"
+                    className="w-1/4 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="zip" className="block text-sm font-medium text-gray-700">
+                  Código Postal*
+                </label>
+                <div className="mt-1">
+                  <input
+                    required
+                    type="text"
+                    name="zip"
+                    id="zip"
+                    autoComplete="postal-code"
+                    className="w-1/4 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+          </div>
+        </div>
+      </form>
     </div>
+  </div>
   )
 }
