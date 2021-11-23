c2d78c4d9098adbce4cdd1391ea983224ad058f7
JanLimpensG <jan_limpens@hotmail.com>
2021-11-22 20:51:44 -0600

    fix: correct alert colors

--- libs/account/ui/src/components/PasswordRecoveryRequestEmailForm/index.tsx
+++ libs/account/ui/src/components/PasswordRecoveryRequestEmailForm/index.tsx
@@ -1,22 +1,22 @@
 //User story: frappe-503
-import { Button, Form, TextField, SpanError } from '@frappe/common/design-system';
+import { Button, Form, TextField, Alert } from '@frappe/common/design-system';
 import { useState } from 'react';
 import { Nullable } from '@frappe/common/utils';
 import { usePasswordRecoveryRequestEmailForm } from '../../hooks';
 import { SubmitHandler } from 'react-hook-form';
 
 export const PasswordRecoveryRequestEmailForm = () => {
-  const [generalError, setGeneralError] = useState<Nullable<string>>(null);
+  const [generalMessage, setGeneralMessage] = useState<Nullable<string>>(null);
   const { sendRequest } = usePasswordRecoveryRequestEmailForm();
   const onSubmit: SubmitHandler<{ readonly email: string}> = async (data, event) => {
     event?.preventDefault();
     sendRequest(data);
-    setGeneralError('Si el correo está registrado en el sistema, se le acaba de mandar una liga para reestablecer la contraseña.');
+    setGeneralMessage('Si el correo está registrado en el sistema, se le acaba de mandar una liga para reestablecer la contraseña.');
   }
 
   return (
     <Form className="flex flex-col w-full p-8" onSubmit={onSubmit}>
-      {generalError && <SpanError message={generalError} />}
+      {generalMessage && <Alert title={"Correo enviado"} body={generalMessage} color={"gray"}/>}
       <TextField label="Email" type="email" name="email" validations={{ required: 'El email es requerido' }} />
       <Button title="Solicitar liga" type="submit" variant="cta" className={'mt-4'} />
     </Form>
