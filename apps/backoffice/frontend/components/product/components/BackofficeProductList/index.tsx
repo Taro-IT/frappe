6cc0c4ca8a46d5422c4f9c221b09059f9794b2b5
Ivan Diaz <A01700486@itesm.mx>
2021-11-23 00:44:23 -0600

    fix: fix lint errors

--- apps/backoffice/frontend/components/product/components/BackofficeProductList/index.tsx
+++ apps/backoffice/frontend/components/product/components/BackofficeProductList/index.tsx
@@ -16,12 +16,17 @@ interface ProductListContentProps {
         <div className="flex flex-col space-y-4 justify-items-center w-full h-full mt-24">
           <AdjustmentsIcon className="text-center h-24 text-gray-400"/>
             <p className="text-gray-400 text-center align-middle">Ups, parece que aún no agregas ningún producto.</p>
-            <p className="text-gray-400 text-center align-middle">Agrega uno usando el botón de '+', localizado arriba a la derecha.</p>
+            <p className="text-gray-400 text-center align-middle">Agrega uno usando el botón de +, localizado arriba a la derecha.</p>
         </div>
       }
         <div className="max-w-screen-2xl grid grid-cols-4 gap-4">
           {products.map(product => (
-            <BackofficeProductCard id={product.id} key={product.id} name={product.name} price={product.price} images={product.images}/>
+            <BackofficeProductCard 
+              id={product.id} 
+              key={product.id} 
+              name={product.name} 
+              price={product.price} 
+              images={product.images}/>
             ) )}
         </div>
     </>
