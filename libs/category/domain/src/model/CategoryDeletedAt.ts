25002281e1db7218109cb8298abad24f5488cffc
Karla Romero <48111726+karlaromerox3@users.noreply.github.com>
2021-11-23 21:51:39 -0600

    fix: remove spaces

--- libs/category/domain/src/model/CategoryDeletedAt.ts
+++ libs/category/domain/src/model/CategoryDeletedAt.ts
@@ -1,4 +1,4 @@
-export class CategoryDeletedAt 
+export class CategoryDeletedAt
 {
   readonly value: Date;
 
--- libs/category/domain/src/model/CategoryIsActive.ts
+++ libs/category/domain/src/model/CategoryIsActive.ts
@@ -1,4 +1,4 @@
-export class CategoryIsActive 
+export class CategoryIsActive
 {
   readonly value: boolean;
 
