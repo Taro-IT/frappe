7cd6cc3e0571d01deb7bbc7c5cffc1be36f5822d
Vladimir Salvador <alonsovla@gmail.com>
2021-11-27 20:08:55 -0600

    fix: modify ecommerce next.config

--- apps/e-commerce/frontend/next.config.js
+++ apps/e-commerce/frontend/next.config.js
@@ -1,15 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const { createSecureHeaders } = require('next-secure-headers')
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false
-  }
+  },
+  poweredByHeader: false,
+  headers: async () => [{ source: '/(.*)', headers: createSecureHeaders() }],
 };
 
 module.exports = withNx(nextConfig);
