14569d642ded3b7830cbdc735fa1d9eecb21394e
EricBL3 <ericbuitron3@gmail.com>
2021-12-06 12:19:05 -0600

    fix: force deploy

--- apps/backoffice/frontend/pages/register.tsx
+++ apps/backoffice/frontend/pages/register.tsx
@@ -2,7 +2,7 @@ import React/*, {useEffect} */ from 'react';
 import { RegisterForm } from '@frappe/account/ui';
 import { AdminLayout } from '../layouts/AdminLayout';
 import { Card, withUserAgent } from '@frappe/common/design-system';
-// import  { withProtectedRoute }  from '../HOC/withProtectedRoute';
+// import  { withProtectedRoute }  from '../HOC/withProtectedRoute'; 
 
 
 const RegisterPage = () => {
