8bc694e0fdd14309e4299e175ab2a4bd23e24420
Ivan Diaz <A01700486@itesm.mx>
2021-12-07 00:53:04 -0600

    fix: fix pr errors

--- apps/e-commerce/frontend/pages/privacy-policy/index.tsx
+++ apps/e-commerce/frontend/pages/privacy-policy/index.tsx
@@ -7,18 +7,18 @@ const PrivacyPolicy = () => {
     <div className="self-center w-2/3 pt-8 pb-8">
       <h1 className="text-center text-xl font-bold">POLÍTICAS DE PRIVACIDAD</h1>
       <p>
-      Dora Elena Gómez Sánchez,  con  domicilio  en 
-      Segunda Cerrada de Romero de Terreros 52, casa 4, Del Valle, Benito Juárez, 03100, Ciudad de México,  es  responsable  
-      de  recabar sus datos personales, del uso que se le dé a los mismos y de su protección.     
+      Dora Elena Gómez Sánchez,  con  domicilio  en
+      Segunda Cerrada de Romero de Terreros 52, casa 4, Del Valle, Benito Juárez, 03100, Ciudad de México,  es  responsable
+      de  recabar sus datos personales, del uso que se le dé a los mismos y de su protección.
         </p>
         <br/>
         <p>
-        Su  información  personal  será  utilizada  para  proveer  los  servicios  y  productos  que  ha 
+        Su  información  personal  será  utilizada  para  proveer  los  servicios  y  productos  que  ha
         solicitado,  informarle sobre cambios en los mismos y evaluar la calidad del servicio
-        que le brindamos. Para las  finalidades  antes  mencionadas,  requerimos  obtener  los  siguientes 
+        que le brindamos. Para las  finalidades  antes  mencionadas,  requerimos  obtener  los  siguientes
         datos  personales:  Nombre completo, domicilio (código postal, estado, país, ciudad o municipio,
-        colonia, localidad, calle, número exterior e interior),  teléfono fijo, correo electrónico, 
-        considerado  como  sensible  según  la  Ley  Federal  de  Protección  de  Datos  Personales  en 
+        colonia, localidad, calle, número exterior e interior),  teléfono fijo, correo electrónico,
+        considerado  como  sensible  según  la  Ley  Federal  de  Protección  de  Datos  Personales  en
         Posesión de los Particulares.
 
         </p>
@@ -37,12 +37,12 @@ const PrivacyPolicy = () => {
         dentro  y  fuera  del  país,  por  personas  distintas  a  esta  empresa.  En  ese  sentido,
         su  información  puede  ser  compartida con empresas de envíos de paquetes nacionales. Si usted no
         manifiesta su oposición para que sus datos personales sean transferidos,
-        se entenderá que ha otorgado su consentimiento para ello. 
+        se entenderá que ha otorgado su consentimiento para ello.
         </p>
         <br/>
         <p>
         Si usted desea dejar de recibir mensajes promocionales de nuestra parte puede solicitarlo a través
-        de nuestra página de contacto: <a href="store.cinica.mx/contact" >store.cinica.mx/contact.</a> 
+        de nuestra página de contacto: <a href="store.cinica.mx/contact" >store.cinica.mx/contact.</a>
         </p>
         <p>
         Cualquier modificación a este aviso de privacidad podrá consultarla por este mismo medio.
