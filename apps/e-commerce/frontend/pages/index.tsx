03974b53791266f3e430305dbb2e18e655928433
JanLimpensG <jan_limpens@hotmail.com>
2021-12-10 20:25:51 -0600

    feat: started new landing  page

--- apps/e-commerce/frontend/pages/index.tsx
+++ apps/e-commerce/frontend/pages/index.tsx
@@ -1,50 +1,39 @@
-import { Banner, EcommerceLayout, withUserAgent } from '@frappe/common/design-system';
-import {GiPriceTag} from 'react-icons/gi';
-import {BiSupport} from 'react-icons/bi';
+import { EcommerceLanding, EcommerceLayout, withUserAgent } from '@frappe/common/design-system';
+import { GiPriceTag } from 'react-icons/gi';
+import { BiSupport } from 'react-icons/bi';
 import { MdDeliveryDining } from 'react-icons/md';
 import { FaCreativeCommonsNc } from 'react-icons/fa';
-import styles  from '../styles/home.module.scss';
+import styles from '../styles/home.module.scss';
+import React from 'react';
 
 const HomePage = () => (
   <div>
-    <Banner image={'https://cinica.mx/wp-content/uploads/2021/07/Copia-de-614A9813.jpg'}/>
-    <div className="flex flex-row justify-around ">
-      <ul className="flex flex-col justify-evenly">
-          <li className="flex">
-            {<MdDeliveryDining size={70} className={styles['icons']}/>}
-            <div className="flex flex-col justify-center ">
-            <b>ENVÍO A DOMICILIO.</b>
-            <p className={styles['p']}>Recibe tu solicitud donde te encuentres.</p>
-            </div>
-          </li>
-          <li className="flex">
-              <BiSupport size={70} className={styles['icons']}/>
-            <div className="flex flex-col justify-center ">
-            <b>ATENCIÓN A CLIENTES.</b>
-            <p className={styles['p']}>Trato personalizado a tu disposición.</p>
-            </div>
-          </li>
-          <li className="flex">
-            <GiPriceTag size={70} className={styles['icons']}/>
-            <div className="flex flex-col justify-center ">
-              <b>CUIDANDO TU ECONOMÍA</b>
-              <p className={styles['p']}>Los mejores precios del mercado.</p>
-            </div>
-          </li>
-          <li className="flex">
-            <FaCreativeCommonsNc size={70} className={styles['icons']}/>
-            <div className="flex flex-col justify-center ">
-              <b>CONVIÉRTETE EN SOCIO</b>
-              <p className={styles['p']}>Recibe precios especiales.</p>
-            </div>
-          </li>
-        </ul>
-      <div className="w-1/4" >
-        <img src="https://cinica.mx/wp-content/uploads/2021/07/PHOTO-2020-07-29-18-00-33.jpg" />
+    <EcommerceLanding />
+    <hr className="mb-5 mt-10" />
+    <div className="grid grid gap-4 grid-cols-4 mb-10 ml-10">
+      <div>
+        {<MdDeliveryDining size={70} className={styles['icons']} />}
+        <b>ENVÍO A DOMICILIO.</b>
+        <p className={styles['p']}>Recibe tu solicitud donde te encuentres.</p>
+      </div>
+      <div>
+        <BiSupport size={70} className={styles['icons']} />
+        <b>ATENCIÓN A CLIENTES.</b>
+        <p className={styles['p']}>Trato personalizado a tu disposición.</p>
+      </div>
+      <div>
+        <GiPriceTag size={70} className={styles['icons']} />
+        <b>CUIDANDO TU ECONOMÍA</b>
+        <p className={styles['p']}>Los mejores precios del mercado.</p>
+      </div>
+      <div>
+        <FaCreativeCommonsNc size={70} className={styles['icons']} />
+        <b>CONVIÉRTETE EN SOCIO</b>
+        <p className={styles['p']}>Recibe precios especiales.</p>
       </div>
     </div>
   </div>
-)
+);
 
 HomePage.Layout = EcommerceLayout;
 
